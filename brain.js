8fefb86b2a28afd55628a6a259afb1583ca05504
TinyNight <zblcm3@gmail.com>
2017-09-21 08:04:08 -0400

    no message

---
+++ brain.js
@@ -0,0 +1,313 @@
+var Brain = {};
+Brain.weight_table = [1024, 256, 64, 16, 4, 1];
+
+Brain.Winable = function(x_list, y_list) {
+	this.x_list = x_list;
+	this.y_list = y_list;
+	/*
+	this.player_1_cached = false;
+	this.player_2_cached = false;
+	this.player_1_cache = 0;
+	this.player_2_cache = 0;
+	*/
+	
+	this.make_nodes = function() {
+		this.nodes = [];
+		var node;
+		var i;
+		for (i = 0; i < x_list.length; i ++) {
+			node = Brain.field[x_list[i]][y_list[i]];
+			node.winables.push(this);
+			this.nodes.push(node);
+		}
+	}
+	this.player_missing = function(player) {
+		
+		/*
+		// check cache.
+		if ((player == 1) && (this.player_1_cached)) {
+			return this.player_1_cache;
+		}
+		if ((player != 1) && (this.player_2_cached)) {
+			return this.player_2_cache;
+		}
+		*/
+		
+		var node_index = 0;
+		var node;
+		var missing = 5;
+		for (node_index in this.nodes) {
+			node = this.nodes[node_index];
+
+			if (node.player == player) {
+				// get one more chess in this winable.
+				missing = missing - 1;
+			}
+			else {
+				if (node.player != 0) {
+					// impossible to finish this winable.
+					/*
+					if (player == 1) {
+						this.player_1_cached = true;
+						this.player_1_cache = -1;
+					}
+					else {
+						this.player_2_cached = true;
+						this.player_2_cache = -1;
+					}
+					*/
+					return -1;
+				}
+			}
+		}
+		/*
+		if (player == 1) {
+			this.player_1_cached = true;
+			this.player_1_cache = missing;
+		}
+		else {
+			this.player_2_cached = true;
+			this.player_2_cache = missing;
+		}
+		*/
+		return missing;
+	}
+	this.fetch_nodes = function(player) {
+		var node_index = 0;
+		var node;
+		var nodes = [];
+		for (node_index in this.nodes) {
+			node = this.nodes[node_index];
+
+			if (node.player == player) {
+				nodes.push(node);
+			}
+		}
+		return nodes;
+	}
+	
+	this.make_nodes();
+}
+
+Brain.Node = function(x, y) {
+	this.x = x;
+	this.y = y;
+	this.winables = [];
+	this.player = 0;
+	
+	this.get_raw_weight = function(player) {
+		if (this.player != 0) {
+			return 0;
+		}
+		var w = 0;
+		var i;
+		var winable;
+		var missing;
+		for (i = 0; i < this.winables.length; i ++) {
+			winable = this.winables[i];
+			missing = winable.player_missing(player);
+			if (missing >= 0) {
+				w = w + Brain.weight_table[missing];
+			}
+		}
+		return w;
+	}
+	
+}
+
+Brain.fetch_winables = function(player, missing) {
+	var winables = [];
+	for (i = 0; i < Brain.winables.length; i ++) {
+		winable = Brain.winables[i];
+		if (winable.player_missing(player) == missing) {
+			winables.push(winable);
+		}
+	}
+	return winables;
+}
+Brain.max_winable_number = function(player) {
+	var i;
+	var c;
+	var m = 6;
+	var winable;
+	for (i = 0; i < Brain.winables.length; i ++) {
+		winable = Brain.winables[i];
+		c = winable.player_missing(player);
+		if ((c >= 0) && (c < m)) {
+			m = c;
+		}
+	}
+	return m;
+}
+
+Brain.assemble_field = function() {
+	// Initialize.
+	Brain.field = [];
+	var x;
+	var y;
+	var line;
+	for (x = 0; x < 15; x ++) {
+		line = [];
+		for (y = 0; y < 15; y ++) {
+			line.push(new Brain.Node(x, y));
+		}
+		Brain.field.push(line);
+	}
+	
+	// Put down chess.
+	var i;
+	for (i = 0; i < Brain.x_list.length; i ++) {
+		Brain.field[Brain.x_list[i]][Brain.y_list[i]].player = 1 - ((i % 2) * 2);
+	}
+	
+	// Make winables.
+	Brain.winables = [];
+	for (x = 0; x < 15; x ++) {
+		for (y = 2; y < 13; y ++) {
+			Brain.winables.push(new Brain.Winable([x, x, x, x, x], [y - 2, y - 1, y, y + 1, y + 2]));
+		}
+	}
+	for (x = 2; x < 13; x ++) {
+		for (y = 0; y < 15; y ++) {
+			Brain.winables.push(new Brain.Winable([x - 2, x - 1, x, x + 1, x + 2], [y, y, y, y, y]));
+		}
+	}
+	for (x = 2; x < 13; x ++) {
+		for (y = 2; y < 13; y ++) {
+			Brain.winables.push(new Brain.Winable([x - 2, x - 1, x, x + 1, x + 2], [y - 2, y - 1, y, y + 1, y + 2]));
+			Brain.winables.push(new Brain.Winable([x + 2, x + 1, x, x - 1, x - 2], [y - 2, y - 1, y, y + 1, y + 2]));
+		}
+	}
+}
+
+Brain.calculate_accurate_weight = function(ratioA, ratioD) {
+	var x;
+	var y;
+	var i;
+	var j;
+	var node;
+	var node_sub;
+	var nodes = [];
+	var sum_sub_weight;
+	var neg_weight;
+		
+	for (x = 0; x < 15; x ++) {
+		for (y = 0; y < 15; y ++) {
+			node = Brain.field[x][y];
+			if (node.player == Brain.playerN) {
+				nodes.push(node);
+			}
+		}
+	}
+	for (i = 0; i < nodes.length; i ++) {
+		node = nodes[i];
+		node.raw_weight = (node.get_raw_weight(Brain.playerS) * ratioA) + (node.get_raw_weight(Brain.playerE) * ratioD);
+	}
+	for (i = 0; i < nodes.length; i ++) {
+		node = nodes[i];
+		node.player = Brain.playerS;
+		sum_sub_weight = 0;
+		for (j = 0; j < nodes.length; j ++) {
+			if (i != j) {
+				node_sub = nodes[j];
+				node_sub.sub_weight = (node_sub.get_raw_weight(Brain.playerS) * ratioA) + (node_sub.get_raw_weight(Brain.playerE) * ratioD);
+				sum_sub_weight = sum_sub_weight + node_sub.sub_weight;
+			}
+		}
+		node.player = Brain.playerN;
+		for (j = 0; j < nodes.length; j ++) {
+			if (i != j) {
+				node_sub = nodes[j];
+				node_sub.prob = node_sub.sub_weight / sum_sub_weight;
+			}
+		}
+		neg_weight = 0;
+		for (j = 0; j < nodes.length; j ++) {
+			if (i != j) {
+				node_sub = nodes[j];
+				neg_weight = neg_weight + (node_sub.prob * node_sub.raw_weight);
+			}
+		}
+		node.acc_weight = node.raw_weight - neg_weight;
+	}
+}
+
+Brain.fetch_accurate_nodes = function() {
+	var max = 0;
+	var nodes = [];
+	var x;
+	var y;
+	var node;
+	
+	for (x = 0; x < 15; x ++) {
+		for (y = 0; y < 15; y ++) {
+			node = Brain.field[x][y];
+			if (node.player == Brain.playerN) {
+				if (node.acc_weight > max) {
+					nodes = [];
+					max = node.acc_weight;
+				}
+				if (node.acc_weight == max) {
+					nodes.push(node);
+				}
+			}
+		}
+	}
+	
+	return nodes;
+}
+
+// return [x, y];
+Brain.main = function(x_list, y_list) {
+	Brain.x_list = x_list;
+	Brain.y_list = y_list;
+	
+	// First to play.
+	if (x_list.length == 0) {
+		return [7, 7];
+	}
+	
+	Brain.playerN = 0;
+	if (x_list.length % 2 == 0) {
+		Brain.playerS = 1;
+		Brain.playerE = -1;
+	}
+	else {
+		Brain.playerS = -1;
+		Brain.playerE = 1;
+	}
+	Brain.assemble_field();
+	
+	var miswinS = Brain.max_winable_number(Brain.playerS);
+	var miswinE = Brain.max_winable_number(Brain.playerE);
+	
+	// 自己有活4或冲4.
+	if (miswinS == 1) {
+		return Brain.almost_win();
+	}
+	
+	// 对方有活4或冲4.
+	if (miswinE == 1) {
+		return Brain.almost_lose();
+	}
+	
+	return Brain.weighted_answer();
+};
+
+Brain.almost_win = function() {
+	var winables = Brain.fetch_winables(Brain.playerS, 1);
+	var node = winables[0].fetch_nodes(Brain.playerN)[0];
+	return [node.x, node.y];
+}
+Brain.almost_lose = function() {
+	var winables = Brain.fetch_winables(Brain.playerE, 1);
+	var node = winables[0].fetch_nodes(Brain.playerN)[0];
+	return [node.x, node.y];
+}
+Brain.weighted_answer = function() {
+	Brain.calculate_accurate_weight(1, 1);
+	var nodes = Brain.fetch_accurate_nodes();
+	var index = Math.floor(Math.random() * nodes.length);
+	var node = nodes[index];
+	return [node.x, node.y];
+}
---
+++ viewhandler.js
@@ -0,0 +1,269 @@
+var ViewHandler = {};
+
+ViewHandler.GRID_SIZE = 30;
+ViewHandler.DOT_RADIUS = 4;
+ViewHandler.INIT_SIZE = 30;
+
+ViewHandler.first_player = true;
+ViewHandler.current_player = true;
+
+ViewHandler.STYLE_LINE = "rgba(0, 0, 0, 1)";
+ViewHandler.STYLE_BACKGROUND = "rgba(223, 255, 223, 1)";
+
+ViewHandler.STYLE_PLAYER_1_1 = "rgba(0, 0, 0, 1)";
+ViewHandler.STYLE_PLAYER_1_2 = "rgba(255, 0, 0, 1)";
+ViewHandler.STYLE_PLAYER_1_3 = "rgba(255, 0, 0, 0)";
+
+ViewHandler.STYLE_PLAYER_2_1 = "rgba(255, 255, 255, 1)";
+ViewHandler.STYLE_PLAYER_2_2 = "rgba(0, 0, 255, 1)";
+ViewHandler.STYLE_PLAYER_2_3 = "rgba(0, 0, 255, 0)";
+
+ViewHandler.assemble_playfield = function() {
+	ViewHandler.chess_x = [];
+	ViewHandler.chess_y = [];
+	ViewHandler.playfield = [];
+	var x;
+	var y;
+	var line;
+	for (x = 0; x < 15; x ++) {
+		line = [];
+		for (y = 0; y < 15; y ++) {
+			line.push(0);
+		}
+		ViewHandler.playfield.push(line);
+	}
+	ViewHandler.current_player = ViewHandler.first_player;
+	ViewHandler.first_player = !ViewHandler.first_player;
+	
+	ViewHandler.on_draw(ViewHandler.ctx);
+	
+	if (!ViewHandler.current_player) {
+		ViewHandler.computer_move();
+	}
+}
+
+ViewHandler.draw_background_dots = function(ctx, x, y) {
+	var nx = ViewHandler.INIT_SIZE + (ViewHandler.GRID_SIZE * x);
+	var ny = ViewHandler.INIT_SIZE + (ViewHandler.GRID_SIZE * y);
+	
+	ctx.fillStyle = ViewHandler.STYLE_LINE;
+	ctx.beginPath();
+	ctx.arc(nx, ny, ViewHandler.DOT_RADIUS, 0, 2 * Math.PI);
+	ctx.fill();
+}
+ViewHandler.draw_chess = function(ctx, x, y, player) {
+	var nx = ViewHandler.INIT_SIZE + (ViewHandler.GRID_SIZE * x);
+	var ny = ViewHandler.INIT_SIZE + (ViewHandler.GRID_SIZE * y);
+	var grd = ctx.createRadialGradient(nx, ny, 12, nx, ny, 16);;
+	if (player) {
+		grd.addColorStop(0, ViewHandler.STYLE_PLAYER_1_1);
+		grd.addColorStop(0.5, ViewHandler.STYLE_PLAYER_1_2);
+		grd.addColorStop(1, ViewHandler.STYLE_PLAYER_1_3);
+	}
+	else {
+		grd.addColorStop(0, ViewHandler.STYLE_PLAYER_2_1);
+		grd.addColorStop(0.5, ViewHandler.STYLE_PLAYER_2_2);
+		grd.addColorStop(1, ViewHandler.STYLE_PLAYER_2_3);
+	}
+	
+	ctx.fillStyle = grd;
+	ctx.beginPath();
+	ctx.arc(nx, ny, 15, 0, 2 * Math.PI);
+	ctx.fill();
+}
+
+ViewHandler.on_draw = function(ctx) {
+	// Fill Background.
+	ctx.fillStyle = ViewHandler.STYLE_BACKGROUND;
+	ctx.fillRect(0, 0, ViewHandler.canvas.width, ViewHandler.canvas.height);
+	
+	// Draw Lines.
+	var i;
+	var x;
+	var y = ViewHandler.INIT_SIZE + (ViewHandler.GRID_SIZE * 14);
+	for (i = 0; i < 15; i ++) {
+		x = ViewHandler.INIT_SIZE + (ViewHandler.GRID_SIZE * i);
+		ctx.strokeStyle = ViewHandler.STYLE_LINE;
+		ctx.lineWidth = 1;
+		ctx.beginPath();
+		ctx.moveTo(ViewHandler.INIT_SIZE, x);
+		ctx.lineTo(y, x);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(x, ViewHandler.INIT_SIZE);
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
+	
+	// Draw dots.
+	ViewHandler.draw_background_dots(ctx, 3, 3);
+	ViewHandler.draw_background_dots(ctx, 3, 11);
+	ViewHandler.draw_background_dots(ctx, 11, 3);
+	ViewHandler.draw_background_dots(ctx, 11, 11);
+	ViewHandler.draw_background_dots(ctx, 7, 7);
+	
+	// Draw chess.
+	for (i = 0; i < ViewHandler.chess_x.length; i ++) {
+		ViewHandler.draw_chess(ctx, ViewHandler.chess_x[i], ViewHandler.chess_y[i], (i % 2 == 0));
+	}
+};
+
+ViewHandler.array_equals = function(array) {
+	if (array.length < 2) {
+		return true;
+	}
+	var i;
+	for (i = 1; i < array.length; i ++) {
+		if (array[i] != array[0]) {
+			return false;
+		}
+	}
+	return true;
+} 
+
+ViewHandler.check_winner = function() {
+	var pf = ViewHandler.playfield;
+	var x;
+	var y;
+	for (x = 0; x < 15; x ++) {
+		for (y = 0; y < 11; y ++) {
+			if (ViewHandler.array_equals([pf[x][y], pf[x][y + 1], pf[x][y + 2], pf[x][y + 3], pf[x][y + 4]])) {
+				if (pf[x][y] != 0) {
+					return pf[x][y];
+				}
+			} 
+		}
+	}
+	for (x = 0; x < 11; x ++) {
+		for (y = 0; y < 15; y ++) {
+			if (ViewHandler.array_equals([pf[x][y], pf[x + 1][y], pf[x + 2][y], pf[x + 3][y], pf[x + 4][y]])) {
+				if (pf[x][y] != 0) {
+					return pf[x][y];
+				}
+			} 
+		}
+	}
+	for (x = 2; x < 13; x ++) {
+		for (y = 2; y < 13; y ++) {
+			if (ViewHandler.array_equals([pf[x - 2][y - 2], pf[x - 1][y - 1], pf[x][y], pf[x + 1][y + 1], pf[x + 2][y + 2]])) {
+				if (pf[x][y] != 0) {
+					return pf[x][y];
+				}
+			} 
+			if (ViewHandler.array_equals([pf[x + 2][y - 2], pf[x + 1][y - 1], pf[x][y], pf[x - 1][y + 1], pf[x - 2][y + 2]])) {
+				if (pf[x][y] != 0) {
+					return pf[x][y];
+				}
+			} 
+		}
+	}
+	return 0;
+}
+
+ViewHandler.player_move = function(x, y) {
+	if (ViewHandler.add_chess(x, y)) {
+		var winner;
+		ViewHandler.current_player = false;
+		
+		var winner = ViewHandler.check_winner();
+		if (winner != 0) {
+			return ViewHandler.win(winner);
+		}
+		setTimeout(ViewHandler.computer_move, 100);
+	}
+	else {
+		console.log("invalid move");
+	}
+}
+
+ViewHandler.computer_move = function() {
+	var suggestion = Brain.main(ViewHandler.chess_x, ViewHandler.chess_y);
+	var x = suggestion[0];
+	var y = suggestion[1];
+	if (ViewHandler.add_chess(x, y)) {
+		var winner = ViewHandler.check_winner();
+		if (winner != 0) {
+			return ViewHandler.win(winner);
+		}
+		ViewHandler.current_player = true;
+	}
+	else {
+		console.log("BUG: invalid move: [" + x + ", " + y + "]");
+	}
+}
+
+ViewHandler.add_chess = function(x, y) {
+	x = Math.round(x);
+	y = Math.round(y);
+	if ((x < 0) || (x > 14) || (y < 0) || (y > 14)) {
+		return false;
+	}
+	if (ViewHandler.playfield[x][y] != 0) {
+		return false;
+	}
+	if (ViewHandler.chess_x.length % 2 == 0) {
+		ViewHandler.playfield[x][y] = 1;
+	}
+	else {
+		ViewHandler.playfield[x][y] = -1;
+	}
+	ViewHandler.chess_x.push(x);
+	ViewHandler.chess_y.push(y);
+	
+	// Draw Satuation.
+	ViewHandler.on_draw(ViewHandler.ctx);
+	
+	return true;
+}
+
+ViewHandler.win = function(player) {
+	if (player == 1) {
+		setTimeout(ViewHandler.win_1, 100);
+	}
+	else {
+		setTimeout(ViewHandler.win_2, 100);
+	}
+}
+ViewHandler.win_1 = function() {
+	alert("dark player wins");
+	ViewHandler.assemble_playfield();
+}
+ViewHandler.win_2 = function() {
+	alert("light player wins");
+	ViewHandler.assemble_playfield();
+}
+
+ViewHandler.mouse_event = function(key, mx, my) {
+	if (key == 0) {
+		if (ViewHandler.current_player) {
+			var x = Math.round((mx - ViewHandler.INIT_SIZE) / ViewHandler.GRID_SIZE);
+			var y = Math.round((my - ViewHandler.INIT_SIZE) / ViewHandler.GRID_SIZE);
+			/*
+			if (x < 0) {
+				x = 0;
+			}
+			if (x > 14) {
+				x = 14;
+			}
+			if (y < 0) {
+				y = 0;
+			}
+			if (y > 14) {
+				y = 14;
+			}
+			*/
+			ViewHandler.player_move(x, y);
+		}
+		else {
+		}
+	}
+};
+
+ViewHandler.main_loop = function() {
+	while (true) {
+		ViewHandler.assemble_playfield();
+		
+		
+	}
+	
+}
